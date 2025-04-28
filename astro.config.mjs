b23e106c80b571df76c23ce06869c3c4c0107e46
ChimmyTee <jimmyeetran@gmail.com>
2025-04-29 04:53:46 +1000

    added TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,5 +8,6 @@ export default defineConfig({
     base: 'hardly-normal',
     vite: {
         assetsInclude: ['**/*.xlsx'],
+        plugins: [tailwindcss()],
     },
 });
